e5d23a4763c1daa2b0a50157090342f703130301
Samuel-ACH <samuelantoniocastro2021@gmail.com>
2022-04-26 00:36:11 -0600

    Examen

---
+++ Examen3/Interfaces/IUsuarioServicio.cs
@@ -0,0 +1,10 @@
+﻿using Modelos;
+
+namespace Examen3.Interfaces;
+
+public interface IUsuarioServicio
+{
+  
+    Task<IEnumerable<Usuario>> GetLista();
+    Task<Usuario> GetPorCodigo(string codigo);
+}
---
+++ Examen3/Servicios/UsuarioServicio.cs
@@ -0,0 +1,31 @@
+﻿using Examen3.Data;
+using Examen3.Interfaces;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Modelos;
+
+
+namespace Examen3.Servicios;
+
+public class UsuarioServicio : IUsuarioServicio
+{
+    private readonly MySQLConfiguration _configuration;
+    private IUsuarioRepositorio usuarioRepositorio;
+
+    public UsuarioServicio(MySQLConfiguration configuration)
+    {
+        _configuration = configuration;
+        usuarioRepositorio = new UsuarioRepositorio(configuration.CadenaConexion);
+    }
+    public async Task<IEnumerable<Usuario>> GetLista()
+    {
+        return await usuarioRepositorio.GetLista();
+    }
+
+    public async Task<Usuario> GetPorCodigo(string codigo)
+    {
+        return await usuarioRepositorio.GetPorCodigo(codigo);
+    }
+
+ 
+}
